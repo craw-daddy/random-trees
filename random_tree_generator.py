2f6483d980a11e1128517360634ee2eb90c6aa4e
Russell Martin <russell@martinandbrown.com>
2026-03-25 16:03:14 -0400

    Create random_tree_generator.py for generating and visualizing random labeled trees

---
+++ random_tree_generator.py
@@ -0,0 +1,23 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import random
+
+# Function to generate a random labeled tree
+def generate_random_tree(num_nodes):
+    if num_nodes <= 0:
+        return None
+    # Create a random tree using networkx
+    G = nx.random_tree(num_nodes)
+    return G
+
+# Function to visualize the tree
+def visualize_tree(G):
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=700, font_size=10, font_color='black')
+    plt.title('Random Labeled Tree')
+    plt.show()
+
+if __name__ == '__main__':
+    num_nodes = random.randint(1, 10)  # Random number of nodes
+    tree = generate_random_tree(num_nodes)
+    visualize_tree(tree)
